25458445284fa57f3c8e9b4f42b8f92f042d41cf
jbensal <jay.bensal@gmail.com>
2016-12-04 20:54:09 -0800

    sorting output

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import scrapy
 import requests
 import logging
 from lxml import etree, html
@@ -13,16 +12,19 @@ for week in range(season_length):
     tree = html.fromstring(page.content)
     abbr = tree.xpath("//span[@class='abbrev']/text()")
     score = tree.xpath("//td[contains(@class, 'score')]/text()")
-    # print abbr
-    # print score
+
     week_array = []
     for i in range(len(abbr)):
         team = [abbr[i], float(score[i])]
         week_array.append(team)
     week_array.sort(key=lambda tup: tup[1])
-    print week
+
     for i in range(len(week_array)):
         if not week_array[i][0] in total:
             total[week_array[i][0]] = 0
         total[week_array[i][0]] += i
-    print total
+
+sorted_total = sorted(total, key=total.get, reverse=True)
+
+for key in sorted_total:
+    print "Team: {0} Wins: {1}".format(key, total[key])
